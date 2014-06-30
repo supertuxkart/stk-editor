9e61effadf0260f02d3e3f7b915af2847074b75d
Csaba Molnar <elodin@innocent.com>
2014-06-30 11:15:48 +0200

    wrong address fix fix

--- commands/texmodcmd.cpp
+++ commands/texmodcmd.cpp
@@ -92,7 +92,7 @@ void TexModCmd::undo()
     u8 tmp;
     for (u32 i = 0; i < m_mod_count; i++)
     {
-        tmp = *((u8*)((int)m_mod[i].first + img));
+        tmp = *((u8*)((intptr_t)m_mod[i].first + img));
         *((u8*)((int)m_mod[i].first + img)) = m_mod[i].second;
         m_mod[i].second = tmp;
     }
