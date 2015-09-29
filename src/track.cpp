supertuxkart/stk-editor

commit d7a12818dfb13f8dfbfbbb212d7bf2d5c12f9dee

Quick update for new exporter

src/track.cpp
@@ -493,7 +493,7 @@ void Track::build()
     std::ofstream mat;
     mat.open((p + "/materials.xml").c_str());
     mat << "<materials>\n";
-    mat << "  <material name=\"splatt.png\" graphical-effect=\"splatting\"";
+    mat << "  <material name=\"splat.png\" shader=\"splatting\"";
     SMaterial m = m_terrain->getMaterial(0);
     for (int i = 1; i < 5; i++)
     {
@@ -523,7 +523,7 @@ void Track::build()
     stringw track;
     track += "<track  name           = \"";
     track += m_track_name + L"\"\n";
-    track += "        version        = \"5\"\n";
+    track += "        version        = \"6\"\n";
     track += "        groups         = \"made-by-STK-TE\"\n";
     track += "        designer       = \"";
     track += m_designer + "\"\n";
@@ -532,7 +532,12 @@ void Track::build()
     track += "\"\n";
     track += "        screenshot     = \"screenshot.jpg\"\n";
     track += "        smooth-normals = \"true\"\n";
-    track += "        reverse        = \"Y\"\n>\n";
+    track += "        default-number-of-laps = \"3\"\n";
+    track += "        reverse        = \"Y\"\n";
+    track += "        bloom          = \"Y\"\n";
+    track += "        bloom-threshold = \"0.75\"\n";
+    track += "        clouds         = \"N\"\n";
+    track += "        shadows        = \"Y\"\n>\n";
     track += "</track>\n";
 
     PHYSFS_uint64 len = 4 * track.size();

src/mesh/sky.cpp
@@ -86,9 +86,9 @@ stringc Sky::getXmlString()
     stringc h;
 
     s += Editor::getTexStr(m_up) + " " + Editor::getTexStr(m_down) + " " + 
-         Editor::getTexStr(m_left) + " ";
-    s += Editor::getTexStr(m_right) + " " + Editor::getTexStr(m_front) + " " + 
-         Editor::getTexStr(m_back);
+         Editor::getTexStr(m_right) + " ";
+    s += Editor::getTexStr(m_left) + " " + Editor::getTexStr(m_back) + " " + 
+         Editor::getTexStr(m_front);
     s += h + "\"/>\n";
 
     return s;

commit 21dac790a19285257641b809a0d6fc5210577c1c

Fixed terrain texture.
Regression since d7a12818dfb13f8dfbfbbb212d7bf2d5c12f9dee.

src/track.cpp
@@ -493,7 +493,7 @@ void Track::build()
     std::ofstream mat;
     mat.open((p + "/materials.xml").c_str());
     mat << "<materials>\n";
-    mat << "  <material name=\"splat.png\" shader=\"splatting\"";
+    mat << "  <material name=\"splatt.png\" shader=\"splatting\"";
     SMaterial m = m_terrain->getMaterial(0);
     for (int i = 1; i < 5; i++)
     {