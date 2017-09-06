16f351537d979ca2c8bb88c27b252b44e46b51de
Deve <deveee@gmail.com>
2017-09-06 23:58:48 +0200

    Update mingw toolchain files

--- cmake/Toolchain-mingw.cmake
+++ cmake/Toolchain-mingw.cmake
@@ -1,8 +1,5 @@
-### To cross-compile using this toolchain file, use
-###     cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-mingw.cmake
-### command. This file is prepared for Ubuntu 14.10.
-### You may need to write your own toolchain file, 
-### depending on your operating system and compiler.
+# Usage:
+# cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-mingw.cmake 
 
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
@@ -12,12 +9,15 @@ SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc-posix)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++-posix)
 SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
+# figure out folder to look in
+execute_process(COMMAND sh -c "ls /usr/lib/gcc/i686-w64-mingw32/ | grep posix | tr -d '\n'" OUTPUT_VARIABLE MINGW_DEPS_FOLDER)
+
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32 ${PROJECT_SOURCE_DIR}/dependencies)
+SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32 /usr/lib/gcc/i686-w64-mingw32/${MINGW_DEPS_FOLDER}/ ${PROJECT_SOURCE_DIR}/dependencies)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
 # programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ALWAYS)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
